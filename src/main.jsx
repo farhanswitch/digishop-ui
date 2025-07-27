55e174cc63a23a8f7ab4cacd78da0a8660dd6ba9
farhanswitch <muhammad_farhan@outlook.co.id>
2025-07-27 18:29:36 +0700

    Add page Seller Add Product

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import LoginPage from "./pages/Login.jsx";
 import MenuPage from "./pages/Menu.jsx";
 import PrivacyPolicyPage from "./pages/PrivacyPolicy.jsx";
 import RegisterPage from "./pages/Register.jsx";
+import SellerAddProductPage from "./pages/StoreAddProduct.jsx";
 import SellerStoreProfilePage from "./pages/StoreProfile.jsx";
 import SellerProductsPage from "./pages/StoreProducts.jsx";
 createRoot(document.getElementById("root")).render(
@@ -29,6 +30,7 @@ createRoot(document.getElementById("root")).render(
           element={<SellerStoreProfilePage />}
         />
         <Route path="/seller/products" element={<SellerProductsPage />} />
+        <Route path="/seller/product/add" element={<SellerAddProductPage />} />
       </Routes>
     </StrictMode>
   </BrowserRouter>
